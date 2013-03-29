693174a5120aded02f1fbc8530c4f615c6aaa165
sorena100 <sorena100@gmail.com>
2013-03-29 23:27:54 +0430

    Update qa-lang-misc.php

--- qa-include/qa-lang-misc.php
+++ qa-include/qa-lang-misc.php
@@ -1,13 +1,14 @@
 <?php
 	
 /*
-	Question2Answer (c) Gideon Greenspan
+	Question2Answer 1.3.1 (c) 2011, Gideon Greenspan
 
 	http://www.question2answer.org/
 
 	
 	File: qa-include/qa-lang-misc.php
-	Version: See define()s at top of qa-include/qa-base.php
+	Version: 1.3.1
+	Date: 2011-02-01 12:56:28 GMT
 	Description: Miscellaneous language phrases
 
 
@@ -25,68 +26,44 @@
 */
 
 	return array(
-		'block_ip_button' => 'Block IP address',
-		'browse_categories' => 'Browse categories',
-		'captcha_confirm_fix' => 'To avoid this verification in future, please ^5confirm your email address^6.',
-		'captcha_error' => 'Please complete the anti-spam verification',
-		'captcha_label' => 'Anti-spam verification:',
-		'captcha_login_fix' => 'To avoid this verification in future, please ^1log in^2 or ^3register^4.',
-		'feed_a_edited_prefix' => 'Answer edited: ',
-		'feed_a_prefix' => 'Answered: ',
-		'feed_a_reshown_prefix' => 'Answer reshown: ',
-		'feed_a_selected_prefix' => 'Answer selected: ',
-		'feed_c_edited_prefix' => 'Comment edited: ',
-		'feed_c_moved_prefix' => 'Comment moved: ',
-		'feed_c_prefix' => 'Commented: ',
-		'feed_c_reshown_prefix' => 'Comment reshown: ',
-		'feed_closed_prefix' => 'Closed: ',
-		'feed_edited_prefix' => 'Edited: ',
-		'feed_hidden_prefix' => 'Hidden: ',
-		'feed_not_found' => 'Feed not found',
-		'feed_recategorized_prefix' => 'Recategorized: ',
-		'feed_reopened_prefix' => 'Reopened: ',
-		'feed_reshown_prefix' => 'Reshown: ',
-		'feed_retagged_prefix' => 'Retagged: ',
-		'feedback_email' => 'Your email: (optional)',
-		'feedback_empty' => 'Please use this field to send some comments or suggestions',
-		'feedback_message' => 'Your comments or suggestions for ^:',
-		'feedback_name' => 'Your name: (optional)',
-		'feedback_sent' => 'Your message below was sent - thank you.',
-		'feedback_title' => 'Send feedback',
-		'hide_all_ip_button' => 'Hide all posts from this IP',
-		'host_name' => 'Host name:',
+                'Nav_my_details' => 'نمایه',
+                'Nav_all_my_updates' => 'تمامی بروزرسانی ها',
+                'Nav_my_favorites' => 'بروز رسانی های علاقه هایتان',
+                'Nav_my_content' => 'بروز رسانی های نوشته هایتان',
+                'suggest_favorites_add' => 'برای اضافه کردن یک سوال یا موارد دیگر به علاقه هایتان, روی <SPAN CLASS="qa-favorite-image">&nbsp;</SPAN> که در بالای صفحه اش وجود دارد کلیک کنید.',
+                'my_favorites' => 'علاقه های من',
+                'no_favorite_qs' => 'هیچ سوال مورد علاقه ای وجود ندارد',
+                'no_favorite_users' => 'هیچ دوستی وجود ندارد.',
+                'no_favorite_tags' => 'هیچ برچسب مورد علاقه ای وجود ندارد.',
+                'no_favorite_categories' => 'هیچ دسته بندی مورد علاقه ای وجود ندارد.',
+                'suggest_update_favorites' => 'به بروزرسانی های علاقه هایتان مراجعه کنید.',
+                'private_message_title' => 'ارسال یک پیام خصوصی',
+                'my_favorites_title' => 'مورد علاقه های من',
+                'recent_updates_title' => 'به روز رسانی اخیر برای من',
+	        'browse_categories' => 'دسته بندی ها',
+		'block_ip_button' => 'آدرس آی پی بلاک شده',
+		'captcha_confirm_fix' => 'برای نمایش این قابلیت ^5ایمیل فعال سازی را تایید کنید^6.',
+		'captcha_error' => 'کد امنیتی وارد نشده یا ناصحیح است',
+		'captcha_label' => 'کد امنیتی را وارد نمائید',
+		'captcha_login_fix' => 'برای نمایش این قابلیت  ^1وارد شده^2 یا ^3عضو شوید^4.',
+		'feed_a_edited_prefix' => 'پاسخ ادیت شد: ',
+		'feed_a_prefix' => 'پاسخه داده شده: ',
+		'feed_c_edited_prefix' => 'نظر تغییر داده شده : ',
+		'feed_c_prefix' => 'نظر داده شده: ',
+		'feed_edited_prefix' => 'تغییر داده شده ',
+		'feed_not_found' => 'خوراک سایت پیدا نشد',
+		'feedback_email' => 'ایمیل شما: ',
+		'feedback_empty' => 'لطفا از این فیلد برای ارسال نظرات استفاده کنید',
+		'feedback_message' => 'پیام شما درباره ی ^:',
+		'feedback_name' => 'نام شما : ',
+		'feedback_sent' => 'پیام شما ارسال شد - متشکریم.',
+		'feedback_title' => 'ارسال',
+		'host_name' => 'نام میزبان',
 		'matches_blocked_ips' => 'Matches blocked IP addresses:',
-		'message_empty' => 'Please enter your message to send to this user',
-		'message_explanation' => 'This will be sent as a notification from ^. Your email address will not be revealed unless you include it in the message.',
-		'message_for_x' => 'Your message for ^:',
-		'message_limit' => 'Too many private messages sent - please try again in an hour',
-		'message_must_login' => 'Please ^1log in^2 or ^3register^4 to send private messages.',
-		'message_received_x_ago' => 'Received ^ ago:',
-		'message_recent_history' => 'Recent correspondence with ^',
-		'message_sent_x_ago' => 'Sent ^ ago:',
-		'message_sent' => 'Your private message below was sent',
-		'my_favorites_title' => 'My favorites',
-		'nav_all_my_updates' => 'All my updates',
-		'nav_my_content' => 'My content',
-		'nav_my_details' => 'My details',
-		'nav_my_favorites' => 'My favorites',
-		'no_activity_from_x' => 'No activity from ^',
-		'no_favorite_categories' => 'No favorite categories',
-		'no_favorite_qs' => 'No favorite questions',
-		'no_favorite_tags' => 'No favorite tags',
-		'no_favorite_users' => 'No favorite users',
-		'no_recent_updates' => 'No recent updates',
-		'no_updates_content' => 'No recent updates for my content',
-		'no_updates_favorites' => 'No updates for my favorites',
-		'private_message_title' => 'Send a private message',
-		'recent_activity_from_x' => 'Recent activity from ^',
-		'recent_updates_content' => 'Recent updates for my content',
-		'recent_updates_favorites' => 'Recent updates for my favorites',
-		'recent_updates_title' => 'Recent updates for me',
-		'site_in_maintenance' => 'This site is currently down for maintenance - please come back soon.',
-		'suggest_favorites_add' => 'To add a question or other item to your favorites, click the ^ at the top of its page.',
-		'suggest_update_favorites' => 'For more updates, add items to ^1your favorites^2.',
-		'unblock_ip_button' => 'Unblock IP address',
+		'no_activity_from_x' => 'هیچ فعالیتی از ^',
+		'recent_activity_from_x' => 'آخرین فعالیت از سوی ^',
+		'site_in_maintenance' => 'این سایت برای مدتی بسته است - لطفا بعدا برگردید',
+		'unblock_ip_button' => 'در آوردن آی پی از حالت بلاک شده',
 	);
 	
 
