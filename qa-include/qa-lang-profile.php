1c5b1b89b76634ba52cb3e068a64c71583524adc
sorena100 <sorena100@gmail.com>
2013-03-29 23:29:43 +0430

    Update qa-lang-profile.php

--- qa-include/qa-lang-profile.php
+++ qa-include/qa-lang-profile.php
@@ -1,13 +1,14 @@
 <?php
 	
 /*
-	Question2Answer (c) Gideon Greenspan
+	Question2Answer 1.3.1 (c) 2011, Gideon Greenspan
 
 	http://www.question2answer.org/
 
 	
 	File: qa-include/qa-lang-profile.php
-	Version: See define()s at top of qa-include/qa-base.php
+	Version: 1.3.1
+	Date: 2011-02-01 12:56:28 GMT
 	Description: Language phrases for user profile page
 
 
@@ -25,51 +26,49 @@
 */
 
 	return array(
-		'1_chosen_as_best' => ' (1 chosen as best)',
-		'1_down_vote' => '1 down vote',
-		'1_up_vote' => '1 up vote',
-		'1_with_best_chosen' => ' (1 with best answer chosen)',
-		'activity_by_x' => 'Activity by ^',
-		'answers' => 'Answers:',
-		'bonus_points' => 'Bonus points:',
-		'comments' => 'Comments:',
-		'extra_privileges' => 'Extra privileges:',
-		'gave_out' => 'Gave out:',
-		'my_account_title' => 'My account details',
-		'no_posts_by_x' => 'No posts by ^',
-		'permit_anon_view_ips' => 'Viewing IPs of anonymous posts',
-		'permit_close_q' => 'Closing any question',
-		'permit_delete_hidden' => 'Deleting hidden posts',
-		'permit_edit_a' => 'Editing any answer',
-		'permit_edit_c' => 'Editing any comment',
-		'permit_edit_q' => 'Editing any question',
-		'permit_flag' => 'Flagging posts',
-		'permit_hide_show' => 'Hiding or showing any post',
-		'permit_moderate' => 'Approving or rejecting posts',
-		'permit_post_a' => 'Answering questions',
-		'permit_post_c' => 'Adding comments',
-		'permit_post_q' => 'Asking questions',
-		'permit_recat' => 'Recategorizing any question',
-		'permit_retag' => 'Retagging any question',
-		'permit_select_a' => 'Selecting answer for any question',
-		'permit_view_q_page' => 'Viewing question pages',
-		'permit_vote_a' => 'Voting on answers',
-		'permit_vote_down' => 'Voting posts down',
-		'permit_vote_q' => 'Voting on questions',
-		'questions' => 'Questions:',
-		'ranked_x' => ' (ranked #^)',
-		'received' => 'Received:',
-		'recent_activity_by_x' => 'Recent activity by ^',
-		'score' => 'Score:',
-		'send_private_message' => ' - ^1send private message^2',
-		'set_bonus_button' => 'Update bonus',
-		'title' => 'Title:',
-		'user_x' => 'User ^',
-		'voted_on' => 'Voted on:',
-		'x_chosen_as_best' => ' (^ chosen as best)',
-		'x_down_votes' => '^ down votes',
-		'x_up_votes' => '^ up votes',
-		'x_with_best_chosen' => ' (^ with best answer chosen)',
+                'extra_privileges'=>'دسترسی ها',
+                'bonus_points'=>'قدرت امتیاز دهی',
+                'set_bonus_button'=>'تغییر قدرت امتیاز دهی',
+                'permit_view_q_page' => 'مشاهده صفحات سوال:',
+                'permit_post_q' => 'درخواست سوال:',
+                'permit_post_a' => 'پاسخ به پرسش:',
+                'permit_post_c' => 'اضافه کردن نظر:',
+                'permit_vote_q' => 'رای دادن در مورد مسائل:',
+                'permit_vote_a' => 'رای گیری در پاسخ:',
+                'permit_vote_down' => 'رای دادن نوشته ها به پایین:',
+                'permit_recat' => 'دسته بندی مجدد هر سوال:',
+                'permit_edit_q' => 'ویرایش هر گونه سوال:',
+                'permit_edit_a' => 'ویرایش هر پاسخ:',
+                'permit_edit_c' => 'ویرایش هر نظر:',
+                'permit_close_q' => 'بستن هر گونه سوال:',
+                'permit_select_a' => 'انتخاب پاسخ برای هر سوال:',
+                'permit_anon_view_ips' => 'مشاهده IP های پستهای نا شناس:',
+                'permit_flag' => 'نوشته های پرچم دار:',
+                'permit_moderate' => 'تائید یا رد نوشته ها:',
+                'permit_hide_show' => 'پنهان کردن یا نشان دادن هر پست:',
+                'permit_delete_hidden' => 'حذف کردن پستهای مخفی:',
+		'1_chosen_as_best' => ' (برترین پاسخ انتخاب شده)',
+		'1_down_vote' => '1 امتیاز منفی',
+		'1_up_vote' => '1 امتیاز مثبت',
+		'1_with_best_chosen' => ' (1 انتخاب بهترین پاسخ)',
+		'activity_by_x' => 'فعالیت توسط ^',
+		'answers' => 'پاسخها:',
+		'comments' => 'نظرات:',
+		'gave_out' => 'پایان یافته:',
+		'my_account_title' => 'اطلاعات حساب کاربری من',
+		'no_posts_by_x' => '^ فاقد پست',
+		'questions' => 'پرسشها:',
+		'ranked_x' => ' (رتبه بندی #^)',
+		'received' => 'دریافتی:',
+		'recent_activity_by_x' => 'آخرین فعالیت توسط ^',
+		'score' => 'امتیاز:',
+		'title' => 'عنوان:',
+		'user_x' => 'کاربر ^',
+		'voted_on' => 'امتیازدهی در:',
+		'x_chosen_as_best' => ' (^ انتخاب بعنوان برترین)',
+		'x_down_votes' => '^ امتیاز منفی',
+		'x_up_votes' => '^ امتیاز مثبت',
+		'x_with_best_chosen' => ' (^ انتخاب بعنوان برترین پاسخ)',
 	);
 	
 
